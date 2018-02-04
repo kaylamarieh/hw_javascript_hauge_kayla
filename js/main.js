a756c9a3175e48560ff222fb384fef1a8ee89d3c
Kayla Hauge <kmhauge@ufl.edu>
2018-02-04 11:51:01 -0500

    create a function expression

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        return 'What a short little word!';
+    }
+    if (string.length > 7) {
+        return "I'm sorry, but that's too many to count.";
+    }
+    if (string.length === 7) {
+        return '7, what a perfect choice!';
+    }
+};
+
+numba('7');
+numba('seventy');
+numba('university');
