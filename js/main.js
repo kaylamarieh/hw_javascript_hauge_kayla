23b01db2bf824d8895f0838daa66ca8b6d1c1ac7
Kayla Hauge <kmhauge@ufl.edu>
2018-02-04 11:04:28 -0500

    add and object array to javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: 1, party: 'Republican', yearsOfPresidency: 1},
+    {firstName: 'Barack', lastName: 'Obama', termLength: 2, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush', termLength: 1, party: 'Republican', yearsOfPresidency: 4}
+];
+
+console.log(presidents[2].firstName, presidents[2].lastName);
